1b83675b6fc4d0b346c92e7c444d4382d88db286
Claudio Acioli <kllaudyo@gmail.com>
2017-07-13 20:36:00 -0300

    Adicionando Servicos

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FotoComponent } from '../../foto/foto.component';
-import { Http, Headers } from '@angular/http';
+import { FotoService } from '../../foto/foto.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
     selector: 'app-cadastro',
@@ -11,11 +12,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
-    http: Http;
     formulario: FormGroup;
+    service: FotoService;
 
-    constructor(http: Http, builder: FormBuilder) {
-        this.http = http;
+    constructor(service: FotoService, builder: FormBuilder) {
+        this.service = service;
         this.formulario = builder.group({
             titulo: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
             url: ['', Validators.required],
@@ -25,24 +26,10 @@ export class CadastroComponent {
 
     public cadastrar(event) {
         event.preventDefault();
-        console.log(this.foto);
-        const header = new Headers();
-        header.append('Content-Type', 'application/json');
-        this.http
-            .post('http://localhost:3000/v1/fotos', JSON.stringify(this.foto), {headers: header})
-            .subscribe(
-                function(){
-                    this.foto = new FotoComponent();
-                    console.log('Foto salva com sucesso');
-                },
-                function(error){
-                    console.log(error);
-                }
-                // () => {
-                //     this.foto = new FotoComponent();
-                //     console.log('Foto salva com sucesso');
-                // },
-                // error => console.log(error)
-            );
+        this.service.cadastro(this.foto).subscribe(() => {
+            this.foto = new FotoComponent();
+            console.log('Foto salva com sucesso');
+        }, error => console.log(error));
+
     }
 }
--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-
+import { FotoComponent } from '../../foto/foto.component';
+import { FotoService } from '../../foto/foto.service';
 
 @Component({
     selector: 'app-listagem',
@@ -11,21 +11,11 @@ export class ListagemComponent {
 
     fotos: Object[] = [];
 
-    constructor(http: Http) {
-        http.get('http://localhost:3000/v1/fotos')
-            .map(response => response.json())
-            // .filter(foto => (foto.titulo || '').length > 0)
-            .subscribe(
-                fotos => {
-                     // for (let i = 0, len = fotos.length; i < len; i++) {
-                     //     if (fotos[i].titulo.length === 0) { continue };
-                     //     this.fotos[this.fotos.length++] = fotos[i];
-                     // }
-                    // console.log('total ' + this.fotos.length);
-                    this.fotos = fotos.filter(foto => (foto.titulo || '').length > 0 );
-                },
-                error => alert(error)
-            );
+    constructor(service: FotoService) {
+        service.lista().subscribe(
+            fotos => this.fotos = fotos,
+            error => console.log(error)
+        );
     }
 
 }
