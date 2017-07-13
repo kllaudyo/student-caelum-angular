25947350c94a68a662768ce44d3e3dd31a96fac1
Claudio Acioli <kllaudyo@gmail.com>
2017-07-12 22:12:24 -0300

    Adicionando um filtro de programação funcional para visualizar apenas fotos com titulos definidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 
+
 @Component({
     selector: 'app-listagem',
     templateUrl: './listagem.component.html'
@@ -13,8 +14,16 @@ export class ListagemComponent {
     constructor(http: Http) {
         http.get('http://localhost:3000/v1/fotos')
             .map(response => response.json())
+            // .filter(foto => (foto.titulo || '').length > 0)
             .subscribe(
-                fotos => this.fotos = fotos,
+                fotos => {
+                     // for (let i = 0, len = fotos.length; i < len; i++) {
+                     //     if (fotos[i].titulo.length === 0) { continue };
+                     //     this.fotos[this.fotos.length++] = fotos[i];
+                     // }
+                    // console.log('total ' + this.fotos.length);
+                    this.fotos = fotos.filter(foto => (foto.titulo || '').length > 0 );
+                },
                 error => alert(error)
             );
     }
