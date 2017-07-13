ba5325db5fc492110d0b98e60980313b7e9b92eb
Claudio Acioli <kllaudyo@gmail.com>
2017-07-12 21:11:53 -0300

    adicionando chamada ao post da api restful

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FotoComponent } from '../../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     selector: 'app-cadastro',
@@ -9,9 +10,32 @@ import { FotoComponent } from '../../foto/foto.component';
 export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
+    http: Http;
+
+    constructor(http: Http) {
+        this.http = http;
+    }
 
     public cadastrar(event) {
         event.preventDefault();
         console.log(this.foto);
+        const header = new Headers();
+        header.append('Content-Type', 'application/json');
+        this.http
+            .post('http://localhost:3000/v1/fotos', JSON.stringify(this.foto), {headers: header})
+            .subscribe(
+                function(){
+                    this.foto = new FotoComponent();
+                    console.log('Foto salva com sucesso');
+                },
+                function(error){
+                    console.log(error);
+                }
+                // () => {
+                //     this.foto = new FotoComponent();
+                //     console.log('Foto salva com sucesso');
+                // },
+                // error => console.log(error)
+            );
     }
 }
